d84a338c8b7e3867747507f393fe8ac466db3b75
xandegrawe <xandegrawe@gmail.com>
2023-11-27 18:32:56 -0300

    Criação de novas rotas

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     resources :customers, only: [:index, :show, :create, :update, :destroy]
     resources :providers, only: [:index, :show, :create, :update, :destroy]
+    resources :bank_accounts, only: [:index, :show, :create, :update, :destroy]
+    resources :bank_invoices, only: [:index, :show, :create, :update, :destroy]
+    resources :categories, only: [:index, :show, :create]
+    resources :people, only: [:index, :show]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
