a19ca160b4cf102876cfd9d07376eb1fa84701fa
xandegrawe <xandegrawe@gmail.com>
2023-11-27 18:31:24 -0300

    Criação de novos serializers e atualização nos existentes

--- backend/app/serializers/provider_serializer.rb
+++ backend/app/serializers/provider_serializer.rb
@@ -1,3 +1,3 @@
-class CustomerSerializer < ActiveModel::Serializer
+class ProviderSerializer < ActiveModel::Serializer
   attributes  :id, :cnpj, :person_id, :name, :phone, :email, :street, :number, :cep, :observation, :address_type, :city, :state, :neighborhood
 end
