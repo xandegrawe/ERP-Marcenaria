f549420ef551176fd11d58dc4d40b8b0b1d3f773
xandegrawe <xandegrawe@gmail.com>
2023-12-02 18:08:46 -0300

    adição regra rubocop e ajustes de gem

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -14,7 +14,7 @@ module Backend
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
-    config.i18n.default_locale = :"pt-BR"
+    config.i18n.default_locale = :'pt-BR'
     config.time_zone = 'Brasilia'
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
